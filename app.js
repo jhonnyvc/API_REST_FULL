ebab270e009eddb7896e74c0daafdb20230a75a3
johnveca <phjohnvecamail.com>
2021-09-24 20:55:29 -0500

    Actualizando variables

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var mysql = require('mysql');
-var cors = require('cors');
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
 
-var app=express();
+const app=express();
 app.use(express.json());
 app.use(cors());
 
 
 //Establecemos los paremetros
-var conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
